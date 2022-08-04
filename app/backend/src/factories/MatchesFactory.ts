80b510205868eb453c794c7317bcc3e5d43f38b2
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-08-04 16:45:35 -0300

    Requisito 26 completo - não é possível criar nova match se um dos times não existe

--- app/backend/src/factories/MatchesFactory.ts
+++ app/backend/src/factories/MatchesFactory.ts
@@ -1,10 +1,12 @@
 import MatchesController from '../controllers/Matches.controller';
 import MatchesService from '../services/Matches.service';
 import MatchesRepository from '../repository/Matches.repository';
+import TeamsRepository from '../repository/Teams.repository';
 
 export default () => {
   const repository = new MatchesRepository();
-  const service = new MatchesService(repository);
+  const teamsRepository = new TeamsRepository();
+  const service = new MatchesService(repository, teamsRepository);
   const controller = new MatchesController(service);
 
   return controller;
--- app/backend/src/services/Matches.service.ts
+++ app/backend/src/services/Matches.service.ts
@@ -1,9 +1,10 @@
-import { IInsertMatch, IMatches, IMatchesModel, IMatchesService } from '../interfaces';
+import { IInsertMatch, IMatches, IMatchesModel, IMatchesService, ITeamsModel } from '../interfaces';
 import HttpException from '../middlewares/HttpExceptions';
 
 export default class MatchesService implements IMatchesService {
-  constructor(private model: IMatchesModel) {
+  constructor(private model: IMatchesModel, private teamsModel: ITeamsModel) {
     this.model = model;
+    this.teamsModel = teamsModel;
   }
 
   findAll(): Promise<IMatches[]> {
@@ -11,9 +12,18 @@ export default class MatchesService implements IMatchesService {
     return matches;
   }
 
-  create(insertedMatch: IInsertMatch): Promise<IMatches> {
+  async create(insertedMatch: IInsertMatch): Promise<IMatches> {
     if (insertedMatch.homeTeam === insertedMatch.awayTeam) {
       throw new HttpException(401, 'It is not possible to create a match with two equal teams');
+    }
+    const searchTeamOne = await this.teamsModel.findByPk(insertedMatch.homeTeam);
+    console.log(searchTeamOne);
+
+    const searchTeamTwo = await this.teamsModel.findByPk(insertedMatch.awayTeam);
+    console.log(searchTeamTwo);
+
+    if (searchTeamOne === null || searchTeamTwo === null) {
+      throw new HttpException(404, 'There is no team with such id!');
     }
     const newMatch = this.model.create(insertedMatch);
     return newMatch;
