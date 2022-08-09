9bc151d5c85f465cd9853b5b07ffcf51c40aa3c1
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-08-08 21:08:43 -0300

    80% de testes completo

--- app/backend/src/tests/Login.test.ts
+++ app/backend/src/tests/Login.test.ts
@@ -21,7 +21,6 @@ describe('Teste da rota POST do login', () => {
     email: 'user@usercom', // SEM @
     password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
   }
-
   const validLoginReturn = {
     id: 5,
     username: 'user',
